49a51501954060708331c6de20f9379def4506e1
PradeepNegi <pradeepnegi2012@gmail.com>
2020-04-22 16:16:15 +0530

    Added axios and hook useeffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,21 @@
-import React from "react";
-class ResourceList extends React.Component {
-  render() {
-    return <div> {this.props.resource}</div>;
-  }
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { getState } from "expect/build/jestMatchersObject";
+const ResourceList = props => {
+  const [resources, setResources] = useState([]);
+
+  const loadData = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${props.resource}`
+    );
+    setResources(response.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return <div> {resources.length}</div>;
+};
 
 export default ResourceList;
