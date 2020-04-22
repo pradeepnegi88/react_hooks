3068fc6c8accc6f6b741af76822b564d5b1943a4
PradeepNegi <pradeepnegi2012@gmail.com>
2020-04-22 15:14:34 +0530

    Added state and clicklistener

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    resource: "posts"
+  };
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: "post" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resource: "todo" })}>
+            Todos
+          </button>
+          {this.state.resource}
+        </div>
+      </div>
+    );
   }
 }
 export default App;
